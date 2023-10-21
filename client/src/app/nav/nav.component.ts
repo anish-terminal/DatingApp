770d9f68619f0580bef81448f88b2670e96288ab
Muhammad Yaseen Alam <yaseenalam1997@gmail.com>
2023-10-22 01:25:15 +0500

    End of section 5

---
+++ client/src/app/nav/nav.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit } from '@angular/core';
+import { AccountService } from '../_services/account.service';
+import { Observable, of } from 'rxjs';
+import { User } from '../_models/user';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent implements OnInit{
+  model : any = {};
+  // loggedIn = false;
+// currentUser$ : Observable<User | null> = of(null)
+
+  constructor(public accountService : AccountService){}
+
+  ngOnInit(): void {
+    //this.getCurrentUser();
+    // this.currentUser$ = this.accountService.currentUser$;
+
+  }
+
+  // getCurrentUser(){
+  //   this.accountService.currentUser$.subscribe({
+  //     next : user => {
+  //       // this.loggedIn = !!user
+  //       if(user){
+  //         this.loggedIn = true;
+  //       }else{
+  //         this.loggedIn=false
+  //       }
+  //     },
+  //     error : error => console.log("err =>",error) 
+  //   })
+  // }
+
+  login() {
+    this.accountService.login(this.model).subscribe({
+      next : response => {
+        console.log(response);
+        // this.loggedIn = true;
+      },
+      error : error => console.log(error)
+      
+    })
+  }
+
+  logout(){
+    this.accountService.logout();
+    // this.loggedIn = false;
+    console.log("logout fucntion is called");
+  }
+
+}
