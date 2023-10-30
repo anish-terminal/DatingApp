5edecddb13ce9ce2f93a8ce197432889d6527685
Muhammad Yaseen Alam <yaseenalam1997@gmail.com>
2023-10-30 20:58:27 +0500

    End Of Section 8

--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,7 @@
+using API.Data;
 using API.Extensions;
 using API.Middleware;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -22,5 +24,20 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
+
+var scope = app.Services.CreateScope();// give access all of the services that we have inside program class
+var services = scope.ServiceProvider;
+
+try
+{
+    var context = services.GetRequiredService<DataContext>(); // making connection with data base or context
+    await context.Database.MigrateAsync(); // updating data context like tables or schema
+    await Seed.SeedUsers(context); // add some initial data in to database by calling this method
+}
+catch (Exception ex)
+{
+    var logger = services.GetService<ILogger<Program>>();
+    logger.LogError(ex, "AN error Occured during migration");
+}
 
 app.Run();
